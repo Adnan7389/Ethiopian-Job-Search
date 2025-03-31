d551e7633be3b2576f6ddb79932d689470027532
Adnan Nuredin <152042446+Adnan7389@users.noreply.github.com>
2025-03-31 22:37:51 +0300

    add forgotted line 'import { useSelector } from 'react-redux';'

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Navbar from './components/Navbar/Navbar';
 import RoleSelection from './pages/RoleSelection/RoleSelection';
 import Login from './pages/Login/Login';
