65629d5ee95226e643e72b2567266f0f872b22fe
Adnan7389! <152042446+Adnan7389@users.noreply.github.com!>
2025-04-30 09:16:46 +0300

    feat: add manage applications feature for employers

--- backend/src/controllers/applicationController.js
+++ backend/src/controllers/applicationController.js
@@ -14,4 +14,49 @@ const getMyApplications = async (req, res) => {
   }
 };
 
-module.exports = getMyApplications;
+const updateApplicationStatus = async (req, res) => {
+  const { id } = req.params; // applicant_id
+  const { status } = req.body;
+
+  try {
+    const validStatuses = ['pending', 'shortlisted', 'rejected', 'interviewed', 'accepted'];
+
+    if (!validStatuses.includes(status)) {
+      return res.status(400).json({ message: 'Invalid status' });
+    }
+
+    const affectedRows = await Applicant.updateStatus(id, status);
+    if (affectedRows === 0) {
+      return res.status(404).json({ message: 'Applicant not found' });
+    }
+
+    res.json({ message: 'Status updated successfully' });
+  } catch (error) {
+    console.error('Error updating application status:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+// Schedule interview
+const scheduleInterview = async (req, res) => {
+  const { id } = req.params; // applicant_id
+  const { date, time, location } = req.body;
+
+  try {
+    if (!date || !time || !location) {
+      return res.status(400).json({ message: 'Missing interview details' });
+    }
+
+    const affectedRows = await Applicant.scheduleInterview(id, { date, time, location });
+    if (affectedRows === 0) {
+      return res.status(404).json({ message: 'Applicant not found' });
+    }
+
+    res.json({ message: 'Interview scheduled successfully' });
+  } catch (error) {
+    console.error('Error scheduling interview:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+module.exports = { getMyApplications, scheduleInterview, updateApplicationStatus};
--- backend/src/routes/applicationRoutes.js
+++ backend/src/routes/applicationRoutes.js
@@ -1,13 +1,16 @@
 // backend/src/routes/applicationRoutes.js
 
 const express = require('express');
-const getMyApplications = require('../controllers/applicationController'); 
+const applicantController = require('../controllers/applicationController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-// Get logged-in user's applications
-router.get('/my-applications', authMiddleware("job_seeker"), getMyApplications);
+// Get logged-in job seeker's applications
+router.get('/my-applications', authMiddleware("job_seeker"), applicantController.getMyApplications);
 
+router.put('/:id/status', authMiddleware("employer"), applicantController.updateApplicationStatus);
+
+router.put('/:id/schedule', authMiddleware("employer"), applicantController.scheduleInterview);
 
 module.exports = router;
